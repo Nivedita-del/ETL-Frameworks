70a4139874901857df10925ad85c1501ff2721ec
vishnu <x3x0n@localhost.localdomain>
2022-10-16 18:45:29 +0530

    Extractor and loaders init

--- rds_to_redshift/src/main/java/Extractors/DBExtractors.java
+++ rds_to_redshift/src/main/java/Extractors/DBExtractors.java
@@ -1,4 +1,27 @@
 package Extractors;
 
+import Connectors.DBConnector;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBExtractors {
+    @Autowired
+    public DBConnector DBConnector;
+
+    public ResultSet RDSExtractor() throws SQLException {
+        Connection con = DBConnector.RdsConnector();
+        Statement statement = con.createStatement();
+        ResultSet res = statement.executeQuery("");
+        return res;
+    }
+    public ResultSet RedshiftExtractor() throws SQLException {
+        Connection con = DBConnector.RedshiftConnector();
+        Statement statement = con.createStatement();
+        ResultSet res = statement.executeQuery("");
+        return res;
+    }
 }
--- rds_to_redshift/src/main/java/Loaders/DBLoaders.java
+++ rds_to_redshift/src/main/java/Loaders/DBLoaders.java
@@ -1,4 +1,31 @@
 package Loaders;
 
+import Connectors.DBConnector;
+import Extractors.DBExtractors;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBLoaders {
+
+    @Autowired
+    public DBExtractors extractors;
+    @Autowired
+    public DBConnector connector;
+
+    public ResultSet RDSLoader() throws SQLException {
+        Connection con = connector.RdsConnector();
+        Statement statement = con.createStatement();
+        ResultSet res = statement.executeQuery("");
+        return res;
+    }
+    public ResultSet RDSExtractor() throws SQLException {
+        Connection con = connector.RdsConnector();
+        Statement statement = con.createStatement();
+        ResultSet res = statement.executeQuery("");
+        return res;
+    }
 }
